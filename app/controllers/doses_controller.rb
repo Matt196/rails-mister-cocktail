2b381765d771ac430ca19af1cb5e2e59c609001a
matthieu reboul <matthieu.reboul@gmail.com>
2017-05-18 16:32:49 +0200

    add dose feature (add, delete)

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,35 @@
 class DosesController < ApplicationController
-  def new
-  end
+  before_action :set_cocktail, only: [:new, :create]
 
-  def create
+  def new
+    @dose = Dose.new
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    @cocktail = @dose.cocktail
+    redirect_to cocktail_path(@cocktail)
   end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
 end
