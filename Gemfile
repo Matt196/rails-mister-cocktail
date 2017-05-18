f3f3d180a0b5bb2d20e2e975e7482057969553b2
matthieu reboul <matthieu.reboul@gmail.com>
2017-05-18 15:10:10 +0200

    add index, show, new, create action for cocktail

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
+gem 'rest-client'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+
+data = RestClient.get "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+parsed = JSON.parse(data)
+drinks = parsed['drinks']
+drinks.each { |item| Ingredient.create(name: item["strIngredient1"]) }
